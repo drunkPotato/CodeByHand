f1df760e8f63c1e2b7dbb1a3551aa3f60a607d3e
drunkPotato <corsinxhidber@gmail.com>
2026-02-16 11:51:46 +0100

    Started Text Analyzer Class

--- Text Analyzer/Analyzer.java
+++ Text Analyzer/Analyzer.java
@@ -7,21 +7,43 @@ public class Analyzer {
 
         String text = myScanner.nextLine();
         System.out.println("Characters in the text: "+ countCharacters(text));
+        System.out.println("Words in the text: " + countWords(text));
+        System.out.println("Sentences in the text: " + countSentences(text));
+        System.out.println("Reversed input: " + reverseText(text));
 
 
         myScanner.close();
     }
 
     public static int countCharacters(String text){
-        
-        // Enter the logic for counting characters in a string here.
-        // I am not sure how this was done ^^ lets look it up later 
-
         int counter = 0;
         for(int i = 0; i < text.toCharArray().length; i ++){
             counter ++;
         }
         
         return counter;
+    }
+
+    public static int countWords(String text){
+        return text.split(" ").length;
+    }
+
+    public static int countSentences(String text){
+        int counter = 0;
+        for(int i = 0; i < countCharacters(text); i++){
+            if(Character.compare(text.charAt(i), '.') == 0 || Character.compare(text.charAt(i), '!') == 0 || 
+                Character.compare(text.charAt(i) , '?') == 0){
+                counter ++;
+            }
+        }
+        return counter;
+    }
+
+    public static String reverseText(String text){
+        String reversed = "";
+        for(int i = text.length() - 1; i >= 0; i--){
+            reversed += text.charAt(i);
+        }
+        return reversed;
     }
 }
